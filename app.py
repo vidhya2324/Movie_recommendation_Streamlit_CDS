38b7b7e15c77240ae30fcec685b75e428e6ba39c
vidhya2324 <vidhyaengineer2022@gmail.com>
2025-12-28 12:48:18 +0530

    Fix sklearn import and Streamlit app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import difflib
 
